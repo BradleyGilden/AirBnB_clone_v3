86fde47017924455cdceda90701d942406543a3b
KevCareSA <kev.mkbn2@gmail.com>
2023-10-27 11:58:00 +0200

    Count objects

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,20 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """retrieve objects of a specific class type"""
+        if cls in classes:
+            for obj in self.all(cls).values():
+                if getattr(obj, 'id', None) == id:
+                    return obj
+        return None
+
+    def count(self, cls=None):
+        """Returns the number of objects in storage matching the given class.
+        If no class is passed, returns the count of all objects in storage
+        """
+        if cls is not None:
+            return len(self.all(cls))
+        else:
+            return len(self.all())
