a8dda46681f8d30ffd2b0d6f326ee8b1b88bcfdd
BradleyGilden <bradleygilden@gmail.com>
2023-10-29 10:41:38 +0200

    use environ var

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,31 +1,34 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+This module contains the principal application
 """
-
-from api.v1.views import app_views
-from flask import Flask, jsonify, make_response
-from flask_cors import CORS
 from models import storage
+from api.v1.views import app_views
+from flask import Flask, make_response, jsonify
+from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
-
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def close_db(obj):
+    """ calls methods close() """
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(err):
-    """returns a custom JSON error page"""
+def page_not_foun(error):
+    """ Loads a custom 404 page not found """
     return make_response(jsonify({"error": "Not found"}), 404)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000', threaded=True)
+if __name__ == "__main__":
+
+    host = getenv('HBNB_API_HOST', default='0.0.0.0')
+    port = getenv('HBNB_API_PORT', default=5000)
+
+    app.run(host, int(port), threaded=True)
